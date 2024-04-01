a498fb8c08d9d55e35a84364f30d7d07687cf59a
karae <karaev.den@gmail.com>
2024-04-01 18:22:27 +0300

    no

--- src/main/java/task4/Application.java
+++ src/main/java/task4/Application.java
@@ -49,8 +49,7 @@ class GPSNavigator {
     private String route;
 
     public GPSNavigator() {
-        this.route = "221b, Baker Street, London to Scotland Yard, 8-10
-        Broadway, London";
+        this.route = "221b, Baker Street, London to Scotland Yard, 8-10 Broadway, London";
     }
 
     public GPSNavigator(String manualRoute) {
