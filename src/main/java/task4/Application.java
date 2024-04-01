0cf06bddb458d485f8dbefc741cc67b6db81d0a7
karae <karaev.den@gmail.com>
2024-04-01 18:20:45 +0300

    no

--- src/main/java/task4/Application.java
+++ src/main/java/task4/Application.java
@@ -1,3 +1,203 @@
 package task4;
 
+enum CarType {
+    CITY_CAR, SPORTS_CAR, SUV
+}
+class Engine {
+    private final double volume;
+    private double mileage;
+    private boolean started;
+
+    public Engine(
+            double volume,
+            double mileage
+    ) {
+        this.volume = volume;
+        this.mileage = mileage;
+    }
+
+    public void on() {
+        started = true;
+    }
+
+    public void off() {
+        started = false;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public void go(double mileage) {
+        if (started) {
+            this.mileage += mileage;
+        } else {
+            System.err.println("Cannot go(), you must start engine first!");
+        }
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+}
+class GPSNavigator {
+
+    private String route;
+
+    public GPSNavigator() {
+        this.route = "221b, Baker Street, London to Scotland Yard, 8-10
+        Broadway, London";
+    }
+
+    public GPSNavigator(String manualRoute) {
+        this.route = manualRoute;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+}
+public enum Transmission {
+    SINGLE_SPEED, MANUAL, AUTOMATIC, SEMI_AUTOMATIC
+}
+class TripComputer {
+
+    private Car car;
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public void showFuelLevel() {
+        System.out.println("Fuel level: " + car.getFuel());
+    }
+
+    public void showStatus() {
+        if (car.getEngine().isStarted()) {
+            System.out.println("Car is started");
+        } else {
+            System.out.println("Car isn't started");
+        }
+    }
+}
+class Car {
+
+    private final CarType carType;
+    private final int seats;
+    private final Engine engine;
+    private final Transmission transmission;
+    private final TripComputer tripComputer;
+    private final GPSNavigator gpsNavigator;
+    private double fuel = 0;
+
+    public Car(
+            CarType carType,
+            int seats,
+            Engine engine,
+            Transmission transmission,
+            TripComputer tripComputer,
+            GPSNavigator gpsNavigator
+    ) {
+        this.carType = carType;
+        this.seats = seats;
+        this.engine = engine;
+        this.transmission = transmission;
+        this.tripComputer = tripComputer;
+        if (this.tripComputer != null) {
+            this.tripComputer.setCar(this);
+        }
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Transmission getTransmission() {
+        return transmission;
+    }
+
+    public TripComputer getTripComputer() {
+        return tripComputer;
+    }
+
+    public GPSNavigator getGpsNavigator() {
+        return gpsNavigator;
+    }
+}
+class CarBuilder {
+
+    private CarType type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    public void setCarType(CarType type) {
+        this.type = type;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    public void setTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public Car getResult() {
+        return new Car(type, seats, engine, transmission, tripComputer,
+                gpsNavigator);
+    }
+
+}
+
+class BuilderService {
+
+    public void exec() {
+        CarBuilder builder = new CarBuilder();
+        builder.setCarType(CarType.SPORTS_CAR);
+        builder.setSeats(2);
+        builder.setEngine(new Engine(3.0, 0));
+        builder.setTransmission(Transmission.SEMI_AUTOMATIC);
+        builder.setTripComputer(new TripComputer());
+        builder.setGPSNavigator(new GPSNavigator());
+        Car car = builder.getResult();
+        System.out.println("Car built:\n" + car.getCarType());
+    }
+}
+
 
