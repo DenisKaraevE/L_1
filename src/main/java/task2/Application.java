649a33b84e2b0d31fd4290047b58e6bf296d1339
karae <karaev.den@gmail.com>
2024-04-01 17:43:37 +0300

    no

---
+++ src/main/java/task2/Application.java
@@ -0,0 +1,95 @@
+package task2;
+
+interface Button {
+    void render();
+}
+interface Dialog {
+    void render();
+}
+class WebButton implements Button {
+    public void render() {
+        System.out.println("render WebButton");
+    }
+}
+class MobButton implement button {
+    public void render() {
+        System.out.println("render MobButton");
+    }
+}
+abstarct class AbstractDialog implements Dialog {
+
+    // Фабричный метод
+    abstarct Button getButton();
+
+    public void render() {
+        Button button = getButton();
+        button.render();
+    }
+
+}
+class WebDialog implements AbstractDialog {
+    private Button getButton() {
+        return new WebButton();
+    }
+}
+class MobDialog extends AbstractDialog {
+    private Button getButton() {
+        return new MobButton();
+    }
+}
+
+class Configuration {
+
+    public final static String int WEB = 1;
+    public final static String int MOB = 2;
+
+    private int platform;
+
+    public int getPlaform() {
+        return platform;
+    }
+
+    public Configuration setPlatform(int platform) {
+        this.platform = platform;
+        return this;
+    }
+
+    public static Configuration initWeb() {
+        return new Configuration().setPlatform(WEB);
+    }
+
+    public static Configuration initMob() {
+        return new Configuration().setPlatform(MOB);
+    }
+
+}
+
+
+class FabricMethodService {
+
+    public void exec() {
+        Configuration configuration = Configuration.initWeb();
+
+        Dialog dialog;
+        switch(conf.getPlatform()) {
+            case (WEB):
+                dialog = new WebDialog();
+            case (MOB):
+                dialog = new MobDialog();
+            default:
+                throw new Exeption("Не известный тип платформы");
+        }
+
+        dialog.render();
+    }
+
+}
+
+class Application {
+
+    public static void main(String[] args) {
+        FabricMethodService service = new FabricMethodService();
+        service.exec();
+    }
+
+}
